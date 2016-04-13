24a20cc2338a64f85d30f94564c9325f87292073
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2016-04-13 13:15:01 +0200

    Update to 160414

--- dbversion-257/application/views/survey/questions/arrays/multiflexi/rows/cells/answer_td_checkboxes.php
+++ dbversion-257/application/views/survey/questions/arrays/multiflexi/rows/cells/answer_td_checkboxes.php
@@ -14,7 +14,7 @@
 ?>
 
 <!-- answer_td_checkboxes -->
-<td data-title="<?php echo $dataTitle;?>" class="answer-cell-6 answer_cell_<?php echo $ld; ?> question-item answer-item <?php echo $answertypeclass;?>-item">
+<td data-title="<?php echo $dataTitle;?>" class="answer-cell-6 answer_cell_<?php echo $ld; ?> question-item answer-item <?php echo $answertypeclass;?>-item checkbox-item checkbox text-center">
 
     <input
         type="hidden"
@@ -45,6 +45,7 @@
                 aelt.value=1;
                 jelt.value=1;
                 <?php echo $checkconditionFunction; ?>(1,'<?php echo $myfname2;?>',aelt.type);
+            }
             else
             {
                 aelt.value='';
@@ -53,10 +54,19 @@
             }
             return true;"
         onchange="checkconditions(this.value, this.name, this.type)"
+        aria-labelledby="label-cbox_<?php echo $myfname2;?>"
         />
 
-        <label class="hide read" for="cbox_<?php echo $myfname2;?>">
+        <label for="cbox_<?php echo $myfname2;?>"></label>
+
+        <!--
+             The label text is provided inside a div,
+             so final user can add paragraph, div, or whatever he wants in the subquestion text
+             This field is related to the input thanks to attribute aria-labelledby
+        -->
+        <div class="hide label-text" id="label-cbox_<?php echo $myfname2;?>">
             <?php echo $dataTitle;?>
-        </label>
+        </div>
+
     </td>
 <!-- end of answer_td_checkboxes -->
