1b1ef2fec0be71730df7df72cdc703ec9f810a8a
LouisGac <louis.gac@limesurvey.org>
2016-12-07 14:55:37 +0100

    Dev: quickfix

--- dbversion-164/application/core/LSYii_Application.php
+++ dbversion-164/application/core/LSYii_Application.php
@@ -36,7 +36,7 @@ class LSYii_Application extends CWebApplication
             $config = require($config);
         }
 
-        Yii::import('application.extensions.phpass.*');
+        Yii::import('application.extensions.phpass.Phpass');
 
         // Add logging of trace
         $config['components']['phpass'] = array(
