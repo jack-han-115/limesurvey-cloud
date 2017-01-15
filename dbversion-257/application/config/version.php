d6526c944af341d4e934d57ea270aaa9c8541f07
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2017-01-15 01:06:43 +0100

    Update to 170115

--- dbversion-257/application/config/version.php
+++ dbversion-257/application/config/version.php
@@ -12,11 +12,11 @@
  *150413
  */
 
-$config['versionnumber'] = '2.58.0';
+$config['versionnumber'] = '2.59.0';
 $config['dbversionnumber'] = 260;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
-$config['assetsversionnumber'] = '46';
+$config['assetsversionnumber'] = '2590';
 return $config;
 
 ?>
