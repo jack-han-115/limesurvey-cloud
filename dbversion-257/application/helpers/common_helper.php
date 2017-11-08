24a85d4e4374bd6c42cf50b7a831908e6bd474da
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2017-11-08 16:30:21 +0100

    Updated customized 257 to build 171108

--- dbversion-257/application/helpers/common_helper.php
+++ dbversion-257/application/helpers/common_helper.php
@@ -6318,5 +6318,24 @@ function humanFilesize($bytes, $decimals = 2)
 
     // LimeService Mod End --------------------------    
 
+    /**
+    * A function to remove ../ or ./ from paths to prevent directory traversal
+    * 
+    * @param mixed $path
+    */
+    function get_absolute_path($path) {
+        $path = str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, $path);
+        $parts = array_filter(explode(DIRECTORY_SEPARATOR, $path), 'strlen');
+        $absolutes = array();
+        foreach ($parts as $part) {
+            if ('.' == $part) continue;
+            if ('..' == $part) {
+                array_pop($absolutes);
+            } else {
+                $absolutes[] = $part;
+            }
+        }
+        return implode(DIRECTORY_SEPARATOR, $absolutes);
+    }    
     
 // Closing PHP tag intentionally omitted - yes, it is okay
