4f378a4deac9ece837c7020fa1fae5e5bcff8060
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2018-07-20 15:49:30 +0200

    Updated customized 345 to 180720

--- dbversion-345/application/config/version.php
+++ dbversion-345/application/config/version.php
@@ -16,5 +16,5 @@ $config['versionnumber'] = '3.13.2';
 $config['dbversionnumber'] = 353;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
-$config['assetsversionnumber'] = '31049';
+$config['assetsversionnumber'] = '32250';
 return $config;
