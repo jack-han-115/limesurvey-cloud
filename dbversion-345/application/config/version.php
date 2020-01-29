40be9e1c42531727be6c0389267ee291bfc7302f
lacrioque <markus.fluer@limesurvey.org>
2020-01-29 10:25:44 +0100

    Updated original to 200129

--- dbversion-345/application/config/version.php
+++ dbversion-345/application/config/version.php
@@ -12,10 +12,10 @@
  */
 
 
-$config['versionnumber'] = '3.21.6';
+$config['versionnumber'] = '3.22.1';
 $config['dbversionnumber'] = 359;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion'] = 3;
-$config['assetsversionnumber'] = '30114';
+$config['assetsversionnumber'] = '30116';
 return $config;
