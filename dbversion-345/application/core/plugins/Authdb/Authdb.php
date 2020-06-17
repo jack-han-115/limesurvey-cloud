a2a624b167e91b6c3311371643fccfe97b117de2
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2020-06-17 10:06:27 +0200

    Fixed issue: Index by curly brackets is deprecated

--- dbversion-345/application/core/plugins/Authdb/Authdb.php
+++ dbversion-345/application/core/plugins/Authdb/Authdb.php
@@ -518,7 +518,7 @@ class Authdb extends AuthPluginBase
 
                     for ($i = 0; $i < 8; $i++)
                     {
-                        $salt .= $APRMD5{rand(0, 63)};
+                        $salt .= $APRMD5[rand(0, 63)];
                     }
 
                     return $salt;
