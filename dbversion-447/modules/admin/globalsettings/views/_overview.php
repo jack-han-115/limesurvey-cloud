5a9902e2f597a20edbe610a4cea433dba148e025
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2021-09-10 10:48:46 +0200

    Fixed issue: Force SSL option was visible

--- dbversion-447/modules/admin/globalsettings/views/_overview.php
+++ dbversion-447/modules/admin/globalsettings/views/_overview.php
@@ -71,7 +71,6 @@ if (Permission::model()->hasGlobalPermission('superadmin', 'read')) {
     ?>
         <p><a href="<?php echo $this->createUrl('admin/globalsettings', array('sa' => 'showphpinfo')) ?>" target="blank" class="button"><?php eT("Show PHPInfo"); ?></a></p>
     <?php
-   =============== LimeService Mod start ============ */
-
 }
+   =============== LimeService Mod start ============ */
 ?>
