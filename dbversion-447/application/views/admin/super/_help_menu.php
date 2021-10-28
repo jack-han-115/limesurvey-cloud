8e81101816eca34c634e9d4c539fe58f219b79f8
Rodrigo Teixeira <rod@naiteon.com>
2021-10-28 20:18:41 -0300

    Show links independently of plan

--- dbversion-447/application/views/admin/super/_help_menu.php
+++ dbversion-447/application/views/admin/super/_help_menu.php
@@ -6,56 +6,18 @@
         <span class="caret"></span>
     </a>
     <!-- LimeService mod start -->
-    <?php if (!isset($currentPlan) || $currentPlan == 'free') { ?>
     <ul class="dropdown-menu larger-dropdown" id="help-dropdown">
         <?php $this->renderPartial( "/admin/super/_tutorial_menu", []); ?>
         <li class="divider"></li>
         <li>
-            <a href="http://manual.limesurvey.org/" target="_blank">
-                <span class="fa fa-question-circle" ></span>
-                <?php eT('LimeSurvey Manual');?>
-                <i class="fa fa-external-link  pull-right"></i>
-            </a>
-        </li>
-        <li>
-            <a href="https://forums.limesurvey.org" target="_blank">
-                <span class="fa-stack halfed">
-                    <span class="fa fa-comment fa-stack-1x" ></span>
-                    <span class="fa fa-group fa-inverse fa-stack-1x halfed" ></span>
-                </span>
-                <?php eT('LimeSurvey Forums');?>
-                <i class="fa fa-external-link  pull-right"></i>
-            </a>
-        </li>
-        <li class="divider"></li>
-        <li>
-            <a href="https://bugs.limesurvey.org/" target="_blank">
-                <span class="fa fa-bug" ></span>
-                <?php eT('Report bugs');?>
-                <i class="fa fa-external-link  pull-right"></i>
-            </a>
-        </li>
-        <li>
-            <a href="https://limesurvey.org/" target="_blank">
-                <span class="fa fa-star" ></span>
-                <?php eT('LimeSurvey Homepage');?>
-                <i class="fa fa-external-link  pull-right"></i>
-            </a>
-        </li>
-    </ul>
-    <?php } else { ?>
-    <ul class="dropdown-menu larger-dropdown" id="help-dropdown">
-        <?php $this->renderPartial( "/admin/super/_tutorial_menu", []); ?>
-        <li class="divider"></li>
-        <li>
-            <a href="https://help.limesurvey.org/portal/en/home" target="_blank">
+            <a href="https://help.limesurvey.org" target="_blank">
                 <span class="fa fa-question-circle" ></span>
                 <?php eT('Help Center');?>
                 <i class="fa fa-external-link  pull-right"></i>
             </a>
         </li>
         <li>
-            <a href="https://help.limesurvey.org/portal/en/kb/getting-started/quick-start" target="_blank">
+            <a href="https://help.limesurvey.org/portal/en/kb/get-started/quick-start-guide" target="_blank">
                 <span class="fa fa-play-circle" ></span>
                 <?php eT('Quick start guide');?>
                 <i class="fa fa-external-link  pull-right"></i>
@@ -63,7 +25,7 @@
         </li>
         <li class="divider"></li>
         <li>
-            <a href="https://faq.limesurvey.org/portal/en/kb/faqs" target="_blank">
+            <a href="https://help.limesurvey.org/portal/en/kb/faqs" target="_blank">
                 <span class="fa fa-list" ></span>
                 <?php eT('FAQ');?>
                 <i class="fa fa-external-link  pull-right"></i>
@@ -77,6 +39,5 @@
             </a>
         </li>
     </ul>
-    <?php } ?>
     <!-- LimeService mod end -->
 </li>
