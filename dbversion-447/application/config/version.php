ce3eda76bbebcba17a2c96f66ccce7f9ac2789dc
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2021-11-29 11:06:43 +0100

    Updated master 447 to 211129

--- dbversion-447/application/config/version.php
+++ dbversion-447/application/config/version.php
@@ -11,10 +11,10 @@
  * See COPYRIGHT.php for copyright notices and details.
  */
 
-$config['versionnumber'] = '5.2.2';
+$config['versionnumber'] = '5.2.4';
 $config['dbversionnumber'] = 477;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30247';
+$config['assetsversionnumber'] = '30249';
 return $config;
