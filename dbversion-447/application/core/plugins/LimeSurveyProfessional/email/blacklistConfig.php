7e8429fdc819097cb369add7033ce00a1ca247a8
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2022-01-14 00:52:08 +0100

    Added a new text to email scam blacklist

--- dbversion-447/application/core/plugins/LimeSurveyProfessional/email/blacklistConfig.php
+++ dbversion-447/application/core/plugins/LimeSurveyProfessional/email/blacklistConfig.php
@@ -6,6 +6,6 @@ $violationThreshold = 3;
 /** @var  array List of blacklisted words/sentences */
 $blacklistEntries = [
     'Your package is waiting to be returned',
-    'ThisIsABlacklistedItemAsWell',
+    'αναστολή',
     'να χρησιμοποιήσετε την κάρτα',
 ];
