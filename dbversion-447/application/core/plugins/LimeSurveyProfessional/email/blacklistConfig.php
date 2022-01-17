9e31db95ab9e4dc7f055cdf732bef24885b0e985
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2022-01-17 14:11:50 +0100

    Added text to blacklist

--- dbversion-447/application/core/plugins/LimeSurveyProfessional/email/blacklistConfig.php
+++ dbversion-447/application/core/plugins/LimeSurveyProfessional/email/blacklistConfig.php
@@ -7,6 +7,7 @@ $violationThreshold = 3;
 $blacklistEntries = [
     'is waiting to be returned',
     'address is incorrect',
+    'Express shipment',
     'επιστροφή',
     'αναστολή',
     'να χρησιμοποιήσετε την κάρτα',
