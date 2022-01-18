fbd8cd0234fde7b03c1b600fd6330b33b1fa1edf
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2022-01-18 11:15:54 +0100

    Updated customized 345 to 220118

--- dbversion-345/application/config/version.php
+++ dbversion-345/application/config/version.php
@@ -12,10 +12,10 @@
  */
 
 
-$config['versionnumber'] = '3.27.24';
-$config['dbversionnumber'] = 365;
+$config['versionnumber'] = '3.27.32';
+$config['dbversionnumber'] = 366;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion'] = 3;
-$config['assetsversionnumber'] = '30216';
+$config['assetsversionnumber'] = '30224';
 return $config;
