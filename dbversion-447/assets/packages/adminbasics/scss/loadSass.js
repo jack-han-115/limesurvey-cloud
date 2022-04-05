f3ff858f32c3a6b24f469630447d67de8087b30e
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2022-04-05 09:58:46 +0200

    Updated customized 447 to 220405

--- dbversion-447/assets/packages/adminbasics/scss/loadSass.js
+++ dbversion-447/assets/packages/adminbasics/scss/loadSass.js
@@ -4,6 +4,7 @@ import '../scss/002-attributeMap.scss';
 import '../scss/003-attributeMapToken.scss';
 import '../scss/004-displayParticipants.scss';
 import '../scss/005-responses.scss';
+import '../scss/006-quickTranslate.scss';
 // LimeService Mod start
 import '../scss/006-datapolicy.scss';
 // LimeService Mod end
