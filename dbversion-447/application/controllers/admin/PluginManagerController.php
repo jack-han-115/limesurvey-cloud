82e7557eb3a7c9edfcaea085c029ddee92c0352b
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2022-04-26 15:41:46 +0200

    Updated customized 447 to 220426

--- dbversion-447/application/controllers/admin/PluginManagerController.php
+++ dbversion-447/application/controllers/admin/PluginManagerController.php
@@ -228,7 +228,7 @@ class PluginManagerController extends SurveyCommonAction
             }
 
             // Load the plugin:
-            App()->getPluginManager()->loadPlugin($oPlugin->name, $pluginId);
+            App()->getPluginManager()->loadPlugin($oPlugin->name, $pluginId, false);
             $result = App()->getPluginManager()->dispatchEvent(
                 new PluginEvent('beforeActivate', $this),
                 $oPlugin->name
@@ -307,7 +307,7 @@ class PluginManagerController extends SurveyCommonAction
         }
 
         $plugin      = Plugin::model()->findByPk($id);
-        $oPluginObject = App()->getPluginManager()->loadPlugin($plugin->name, $plugin->id);
+        $oPluginObject = App()->getPluginManager()->loadPlugin($plugin->name, $plugin->id, false);
 
         if (empty($oPluginObject)) {
             Yii::app()->user->setFlash('error', gT('Could not load plugin'));
