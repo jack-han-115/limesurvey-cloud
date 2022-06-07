17e5b46bdbe5a359ac02b67223b125782ec109ce
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2022-06-07 14:58:23 +0200

    Hotfix: Don't use model class when updating plugin version

--- dbversion-447/application/libraries/PluginManager/PluginBase.php
+++ dbversion-447/application/libraries/PluginManager/PluginBase.php
@@ -442,7 +442,7 @@ abstract class PluginBase implements iPlugin
                         throw new \Exception('Internal error: Found no database entry for plugin id ' . $this->id);
                     }
                     $this->checkActive($pluginModel);
-                    $this->saveNewVersion($pluginModel);
+                    $this->saveNewVersion();
                 }
             }
             return true;
@@ -530,13 +530,18 @@ abstract class PluginBase implements iPlugin
 
     /**
      * Saves the new version from config into database
-     * @return boolean
+     * @return void
      */
     protected function saveNewVersion()
     {
-        $pluginModel = \Plugin::model()->findByPk($this->id);
-        $pluginModel->version = (string) $this->config->metadata->version;
-        return $pluginModel->update();
+        \Yii::app()->db->createCommand()->update(
+            '{{plugins}}',
+            ['version' => (string) $this->config->metadata->version],
+            'id = :id',
+            [
+                ':id' => $this->id
+            ]
+        );
     }
 
     /**
