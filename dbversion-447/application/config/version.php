c6c7389bedba42833810ed19e91f92b42157575c
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2022-08-01 10:45:14 +0200

    Updated customized 447 to 220801

--- dbversion-447/application/config/version.php
+++ dbversion-447/application/config/version.php
@@ -11,10 +11,10 @@
  * See COPYRIGHT.php for copyright notices and details.
  */
 
-$config['versionnumber'] = '5.3.25';
+$config['versionnumber'] = '5.3.29';
 $config['dbversionnumber'] = 488;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30287';
+$config['assetsversionnumber'] = '30291';
 return $config;
