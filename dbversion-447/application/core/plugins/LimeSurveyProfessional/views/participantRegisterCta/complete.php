6691c210c94a99220311b325f256135fc2966a0a
Kevin Foster <kevin.foster@limesurvey.org>
2022-08-30 12:22:47 +0100

    LSC-219 - handle translations for branding views

--- dbversion-447/application/core/plugins/LimeSurveyProfessional/views/participantRegisterCta/complete.php
+++ dbversion-447/application/core/plugins/LimeSurveyProfessional/views/participantRegisterCta/complete.php
@@ -2,13 +2,15 @@
     <div class="col-lg-5 col-md-6 col-sm-8 col-xs-12 col-centered">
         <div class="well text-center">
             <h3>
-                Thank you for taking this survey powered by LimeSurvey.
+                <?php eT('Thank you for taking this survey powered by LimeSurvey.'); ?>
             </h3>
             <p>
-                Turn your own questions into answers and start building your own survey today.
+                <?php eT('Turn your own questions into answers and start building your own survey today.'); ?>
             </p>
             <div>
-                <a href="https://www.limesurvey.org/" target="_blank" class="btn btn-primary">Get started now</a>
+                <a href="https://www.limesurvey.org/" target="_blank" class="btn btn-primary">
+                    <?php eT('Get started now'); ?>
+                </a>
             </div>
         </div>
     </div>
--- dbversion-447/application/core/plugins/LimeSurveyProfessional/views/participantRegisterCta/footer.php
+++ dbversion-447/application/core/plugins/LimeSurveyProfessional/views/participantRegisterCta/footer.php
@@ -1,7 +1,7 @@
 <?php if (getGlobalSetting('branding') === '1') : ?>
     <div class="col-md-12">
         <div class="text-right">
-            <a href="https://www.limesurvey.org/" target="_blank" class="btn btn-primary">Made in LimeSurvey</a>
+            <a href="https://www.limesurvey.org/" target="_blank" class="btn btn-primary"><?php eT('Made in LimeSurvey'); ?></a>
         </div>
     </div>
 <?php endif; ?>
