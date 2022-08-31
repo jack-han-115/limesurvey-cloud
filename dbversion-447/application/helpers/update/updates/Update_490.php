fbb81e60e0f251898cbfd846a0d7134ec3b81c30
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2022-08-31 17:46:53 +0200

    Fixed link to old user management module Fixed button box for old instances

--- dbversion-447/application/helpers/update/updates/Update_490.php
+++ dbversion-447/application/helpers/update/updates/Update_490.php
@@ -13,5 +13,8 @@ class Update_490 extends DatabaseUpdateBase
     public function up()
     {
         $this->db->createCommand()->update("{{surveymenu_entries}}", ['title' => 'Privacy policy settings', 'menu_title' => 'Privacy policy', 'menu_description' => 'Edit privacy policy settings'], "name='datasecurity'");
+        // LimeService Mod start
+        $this->db->createCommand()->update("{{boxes}}", ['url' => 'userManagement/index', 'title' => 'User management', 'desc' => 'User management'], "url='admin/user'");
+        // LimeService Mod end
     }
 }
