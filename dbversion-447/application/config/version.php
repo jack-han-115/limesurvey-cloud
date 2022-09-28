f28712a6f71ee95f1cd50ca9712408b813bd4918
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2022-09-28 10:29:27 +0200

    Updated customized 447 to 220928

--- dbversion-447/application/config/version.php
+++ dbversion-447/application/config/version.php
@@ -11,10 +11,10 @@
  * See COPYRIGHT.php for copyright notices and details.
  */
 
-$config['versionnumber'] = '5.4.1';
+$config['versionnumber'] = '5.4.3';
 $config['dbversionnumber'] = 491;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30296';
+$config['assetsversionnumber'] = '30298';
 return $config;
