65ba93a021aff75e53d49cfcdfc489532742cadd
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2022-11-02 11:47:30 +0100

    Updated original 345 to 221102
    # Resolved conflicts:
    #	dbversion-345/application/controllers/admin/update.php

--- dbversion-345/application/config/version.php
+++ dbversion-345/application/config/version.php
@@ -12,10 +12,10 @@
  */
 
 
-$config['versionnumber'] = '3.28.34';
+$config['versionnumber'] = '3.28.36';
 $config['dbversionnumber'] = 366;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion'] = 3;
-$config['assetsversionnumber'] = '30262';
+$config['assetsversionnumber'] = '30264';
 return $config;
