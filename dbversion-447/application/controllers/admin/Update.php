01d01d1cf41a3231974e8f9339808a4f21ea4c4c
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2022-11-02 11:44:08 +0100

    Updated customized 447 to 221102

--- dbversion-447/application/controllers/admin/Update.php
+++ dbversion-447/application/controllers/admin/Update.php
@@ -440,7 +440,7 @@ class Update extends DynamicSurveyCommonAction
 
                     $remove = $updateModel->removeDeletedFiles((array)$changedFiles);
                     if (!$remove->result) {
-                        return $this->_renderErrorString($remove->error);
+                        return $this->renderErrorString($remove->error, $remove->message);
                     };
                     $file = $updateModel->downloadUpdateFile($access_token, $destinationBuild);
                     if ($file->result) {
@@ -698,11 +698,12 @@ class Update extends DynamicSurveyCommonAction
      * @param string $error the error message
      * @return string
      */
-    private function renderErrorString($error)
+    private function renderErrorString($error, $message = null)
     {
             $errorObject = new stdClass();
             $errorObject->result = false;
             $errorObject->error = $error;
+            $errorObject->message = $message;
             return $this->renderError($errorObject);
     }
 
