1326f26992cb306db0f61b287dc9bbf9c122c59f
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2023-02-28 17:46:19 +0100

    Force asset update

--- dbversion-447/application/config/version.php
+++ dbversion-447/application/config/version.php
@@ -16,5 +16,5 @@ $config['dbversionnumber'] = 495;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30322';
+$config['assetsversionnumber'] = '10322';
 return $config;
